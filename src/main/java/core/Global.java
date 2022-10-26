916d2233084a86fc0c1d9381d06c1dfccb0ffc67
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-10-25 21:41:14 -0400

    CODE Add DiceSide enums and randomEnum procedure

--- src/main/java/core/Global.java
+++ src/main/java/core/Global.java
@@ -1,7 +1,19 @@
 package core;
+import java.util.*;
 
 public class Global {
     public static enum CardTypes {
         CHEST,CAPTAIN,SOCERESS,BATTLE,GOLD_CARD,DIAMOND_CARD,MONKEY_CARD,SKULLS_CARD
+    }
+
+    public static enum DiceSide {
+        SKULL,SWORD,MONKEY,DIAMOND,GOLD,PARROT
+    }
+
+    // taken from https://stackoverflow.com/questions/1972392/pick-a-random-value-from-an-enum
+    public static <T extends Enum<?>> T randomEnum(Class<T> clazz){
+        Random r = new Random();
+        int x = r.nextInt(clazz.getEnumConstants().length);
+        return clazz.getEnumConstants()[x];
     }
 }
