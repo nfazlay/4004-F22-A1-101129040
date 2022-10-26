83df75e40d98efde52828ac4ffe7bc1a0cbed8d0
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-10-25 22:46:43 -0400

    U-TEST Player Card test

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -9,4 +9,15 @@ public class PlayerTest extends TestCase {
         Player player = new Player("John Cena");
         assertEquals("John Cena", player.getName());
     }
+
+    public void testPlayerCard () {
+        Player player = new Player("John Cena");
+        assertNull(player.getCard());
+        player.pickCard();
+        assertNotNull(player.getCard());
+        player.clearCard();
+        assertNull(player.getCard());
+        player.setCard(Global.CardTypes.GOLD_CARD);
+        assertNotNull(player.getCard());
+    }
 }
