b3acab610049c89759702401edb185c786eccd9b
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-10-26 16:53:02 -0400

    U-TEST Add reroll tests

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -50,4 +50,38 @@ public class PlayerTest extends TestCase {
         player.roll();
         assertFalse(player.getDiceList().isEmpty());
     }
+
+    public void testReRoll () {
+        List<Dice> diceArray= new ArrayList<Dice>(8);
+        int[] pos = new int[] {1,2,3,4};
+        Dice d1 = new Dice();
+        d1.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d1);
+        Dice d2 = new Dice();
+        d2.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d2);
+        Dice d3 = new Dice();
+        d3.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d3);
+        Dice d4 = new Dice();
+        d4.setDice(Global.DiceSide.PARROT);
+        diceArray.add(d4);
+        Dice d5 = new Dice();
+        d5.setDice(Global.DiceSide.GOLD);
+        diceArray.add(d5);
+        Dice d6 = new Dice();
+        d6.setDice(Global.DiceSide.DIAMOND);
+        diceArray.add(d6);
+        Dice d7 = new Dice();
+        d7.setDice(Global.DiceSide.SWORD);
+        diceArray.add(d7);
+        Dice d8 = new Dice();
+        d8.setDice(Global.DiceSide.MONKEY);
+        diceArray.add(d8);
+
+        player.setDice(diceArray);
+        assertFalse(player.reRoll(pos));
+        pos = new int [] {5,6,7};
+        assertTrue(player.reRoll(pos));
+    }
 }
