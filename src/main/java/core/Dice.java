980303b1cc1c0366350bac9fafd13f117659413d
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-10-25 20:12:48 -0400

    CODE Dice Class

---
+++ src/main/java/core/Dice.java
@@ -0,0 +1,13 @@
+package core;
+
+public class Dice {
+    int side;
+
+    public void setDice(int side) {
+        this.side = side;
+    }
+
+    public int getDice() {
+        return this.side;
+    }
+}
