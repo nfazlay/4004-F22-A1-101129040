efce71465c4693e01ee5d7b42d3eb767a8ea7414
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-10-25 22:47:34 -0400

    CODE Player card functionality

--- src/main/java/core/Player.java
+++ src/main/java/core/Player.java
@@ -1,7 +1,9 @@
 package core;
+import java.util.*;
 
 public class Player {
     private String name = "";
+    private Card card = null;
 
     public Player (String name) {
         this.name = name;
@@ -9,5 +11,22 @@ public class Player {
 
     public String getName () {
         return this.name;
+    }
+
+    public void pickCard () {
+        Global.CardTypes cardType = Global.randomEnum(Global.CardTypes.class);
+        this.card = new Card(cardType);
+    }
+
+    public void clearCard () {
+        this.card = null;
+    }
+
+    public Card getCard () {
+        return this.card;
+    }
+
+    public void setCard (Global.CardTypes cardType) {
+        this.card = new Card(cardType);
     }
 }
