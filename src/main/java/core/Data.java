075d5364feda473d79c01f7f0dac1664df3f2a97
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-11-01 23:19:15 -0400

    CODE toByteArray code

---
+++ src/main/java/core/Data.java
@@ -0,0 +1,20 @@
+package core;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+public abstract class Data {
+
+    public static byte[] toByteArray(Object obj) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        try (ObjectOutputStream os = new ObjectOutputStream(bos)) {
+            os.writeObject(obj);
+        }
+        return bos.toByteArray();
+    }
+}
