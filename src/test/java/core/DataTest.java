f8fab3d1b2415d7ad178faf4c12fddab98f8dcb0
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-11-01 23:24:01 -0400

    U-TEST fromByteArray failed test

--- src/test/java/core/DataTest.java
+++ src/test/java/core/DataTest.java
@@ -4,13 +4,22 @@ import junit.framework.TestCase;
 
 import java.io.IOException;
 
+import static core.Data.fromByteArray;
 import static core.Data.toByteArray;
 
-public class DataTest extends TestCase{
+public class DataTest extends TestCase {
     public void testToByteArray () throws IOException {
         byte[] byteArr;
         String message = new String("Response from server");
         byteArr = toByteArray(message);
         assertEquals(27, byteArr.length);
+    }
+
+    public void testFromByteArray () throws IOException, ClassNotFoundException {
+        byte[] byteArr;
+        String message = new String("Response from server");
+        byteArr = toByteArray(message);
+        String result = (String) fromByteArray(byteArr);
+        assertEquals(message, result);
     }
 }
