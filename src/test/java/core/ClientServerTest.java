3eae2252f6ac65a040b901d025603d0c1a2c7cfc
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-11-03 13:41:20 -0400

    U-TEST test server

---
+++ src/test/java/core/ClientServerTest.java
@@ -0,0 +1,17 @@
+package core;
+
+import junit.framework.TestCase;
+
+import java.io.IOException;
+
+public class ClientServerTest extends TestCase {
+
+    public void testClientServer () throws IOException, InterruptedException, ClassNotFoundException {
+
+        final GameServer server = new GameServer(8080);
+
+        new Thread(server).start();
+
+        assertEquals(0, server.getPlayerList().size());
+    }
+}
