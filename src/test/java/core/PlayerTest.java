0b096748d69d28d06ac50547b24521cb32dbc4f7
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-11-05 14:58:26 -0400

    U-TEST method to check the number of skulls

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -89,4 +89,35 @@ public class PlayerTest extends TestCase {
         player.setIslandOfSkulls(true);
         assertTrue(player.getIslandOfSkulls());
     }
+
+    public void testCheckSkulls () {
+        List<Dice> diceArray= new ArrayList<Dice>(8);
+        Dice d1 = new Dice();
+        d1.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d1);
+        Dice d2 = new Dice();
+        d2.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d2);
+        Dice d3 = new Dice();
+        d3.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d3);
+        Dice d4 = new Dice();
+        d4.setDice(Global.DiceSide.PARROT);
+        diceArray.add(d4);
+        Dice d5 = new Dice();
+        d5.setDice(Global.DiceSide.GOLD);
+        diceArray.add(d5);
+        Dice d6 = new Dice();
+        d6.setDice(Global.DiceSide.DIAMOND);
+        diceArray.add(d6);
+        Dice d7 = new Dice();
+        d7.setDice(Global.DiceSide.SWORD);
+        diceArray.add(d7);
+        Dice d8 = new Dice();
+        d8.setDice(Global.DiceSide.MONKEY);
+        diceArray.add(d8);
+
+        int skulls = player.checkSkulls(diceArray);
+        assertEquals(3, skulls);
+    }
 }
