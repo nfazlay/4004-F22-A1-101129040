1d22ef6a21b1101f5e8d66c12247c45936a749ad
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-11-09 16:11:02 -0500

    REFACTOR chnages in function names, displaying messages

--- src/main/java/core/Player.java
+++ src/main/java/core/Player.java
@@ -8,7 +8,7 @@ public class Player implements Serializable {
     private List<Dice> diceArrayList = new ArrayList<Dice>(8);
     private Card card = null;
     private int score;
-    private int lastScore;
+    public int lastScore;
 
     String userResponse;
     public Player (String name) {
@@ -83,6 +83,7 @@ public class Player implements Serializable {
     }
 
     public void roll() {
+        diceArrayList = new ArrayList<Dice>(8);
         for(int i =0; i < 8; i++) {
             Dice d = new Dice();
             d.roll();
@@ -120,10 +121,10 @@ public class Player implements Serializable {
         return true;
     }
 
-    public int checkSkulls (List<Dice> list) {
+    public int checkNumSide (List<Dice> list, Global.DiceSide side) {
         int count  = 0;
         for (int i =0; i < list.size(); i ++) {
-            if (list.get(i).getDice() == Global.DiceSide.SKULL) {
+            if (list.get(i).getDice() == side) {
                 count++;
             }
         }
@@ -132,7 +133,7 @@ public class Player implements Serializable {
 
     public int islandOfSkullsPoints (List diceArray, Card card) {
         int points = 0;
-        points = checkSkulls(diceArray)*100;
+        points = checkNumSide(diceArray, Global.DiceSide.SKULL)*100;
         if (card.getType() == Global.CardTypes.CAPTAIN) {
             points = points * 2;
         }
@@ -167,7 +168,7 @@ public class Player implements Serializable {
     }
 
     public boolean died (List<Dice> d, Card c, boolean islandOfSkulls) {
-        int numSkulls = checkSkulls(d) + (c.getType() == Global.CardTypes.SKULLS_CARD? c.getNumSkulls() : 0);
+        int numSkulls = checkNumSide(d, Global.DiceSide.SKULL) + (c.getType() == Global.CardTypes.SKULLS_CARD? c.getNumSkulls() : 0);
         if (numSkulls >= 3 && !islandOfSkulls) { //Turn over because got more skulls
             System.out.println(name +": " + "You got three skulls. Turn Over");
             return true;
@@ -177,10 +178,12 @@ public class Player implements Serializable {
 
     public boolean rollSorceres (Scanner sc, List<Dice> diceArray) {
         System.out.print(name +": " + "Please select which dice to reroll or -1 to exit. For example: 1 will set reroll 1st dice: ");
-        int number = Integer.parseInt(sc.nextLine());
-        if (number == -1) {
-            return true;
-        }
+        int number;
+        try {
+            number = Integer.parseInt(sc.nextLine());
+            if (number == -1) {
+                return true;
+            }
         try {
             if (diceArray.get(number).getDice() != Global.DiceSide.SKULL) {
                 System.out.println(name +": " + "Please select a skull. Try again");
@@ -203,8 +206,8 @@ public class Player implements Serializable {
             {
                 numbers[i] = Integer.parseInt(numberStrs[i]);
             }
-            if (numbers.length != 1) {
-                System.out.println(name +": " + "Select only one dice");
+            if (numbers.length < 1 || numbers.length > 8) {
+                System.out.println(name +": " + "Range should be between 1<=x<=8");
                 return false;
             }
             else if (!this.setAside(numbers)) {
@@ -250,7 +253,7 @@ public class Player implements Serializable {
         System.out.println(name +": " + "Rolling the first dice");
         this.roll();
         Global.printDiceList(diceArrayList);
-        int numSkulls = checkSkulls(diceArrayList) + (cardType == Global.CardTypes.SKULLS_CARD? card.getNumSkulls() : 0);
+        int numSkulls = checkNumSide(diceArrayList, Global.DiceSide.SKULL) + (cardType == Global.CardTypes.SKULLS_CARD? card.getNumSkulls() : 0);
         if (numSkulls > 3 && cardType != Global.CardTypes.BATTLE) {
             System.out.println(name +": " + "You have entered the island of skulls");
             islandOfSkulls = true;
@@ -267,15 +270,14 @@ public class Player implements Serializable {
                 }
             }
             System.out.println(name +": " + "Num skulls: " + numSkulls);
-            if (numSkulls >= 3 && !islandOfSkulls) { //Turn over because got more skulls
-                System.out.println(name +": " + "You got three skulls. Turn Over");
+            if (died(diceArrayList, card, islandOfSkulls)) { //Turn over because got more skulls
                 break;
             }
             if (cardType == Global.CardTypes.CHEST) { // Keeping dice in chest
                 System.out.print(name +": " + "Do you want set aside dice? \"Yes\" or \"No\" : ");
                 userResponse = sc.nextLine();
                 if (userResponse.equals("Yes")) {
-                    while(!keepTOChest(sc)){};
+                    while(!keepTOChest(sc)){}
                 }
             }
             System.out.print(name +": " + "Do you want to re-roll the dice? \"Yes\" or \"No\" : ");
@@ -285,25 +287,37 @@ public class Player implements Serializable {
                     System.out.println(name +": " + "Playing Island of skulls");
                     if (this.rollPlay(sc)) {
                         System.out.println(name +": " + "Rolled inside island of dice");
-                        int newNumSkulls = checkSkulls(diceArrayList);
+                        int newNumSkulls = checkNumSide(diceArrayList, Global.DiceSide.SKULL);
                         if (!(newNumSkulls > numSkulls)) {
                             System.out.println(name +": " + "You received less skulls. Kicking out of Island of Skulls");
+                            lastScore = islandOfSkullsPoints(diceArrayList, card);
                             break;
                         }
                     }
                 }
                 else {
-                    if (this.rollPlay(sc)) {
-
-                    }
+                    this.rollPlay(sc);
                 }
             }
             else {
                 Global.printDiceList(diceArrayList);
                 break;
             }
-            numSkulls = checkSkulls(diceArrayList) + (cardType == Global.CardTypes.SKULLS_CARD? card.getNumSkulls() : 0);
+            numSkulls = checkNumSide(diceArrayList, Global.DiceSide.SKULL) + (cardType == Global.CardTypes.SKULLS_CARD? card.getNumSkulls() : 0);
             Global.printDiceList(diceArrayList);
         }
+        System.out.println("Previous score: " + score);
+        if (!islandOfSkulls) {
+            if (checkSeaBattle(diceArrayList, card)) {
+                score += countPoints(diceArrayList, card);
+            }
+            else {
+                System.out.println("You received less swords. All dices ignored and " + card.getSwordPoints()
+                        + " deducted from your score" );
+                score -= card.getSwordPoints();
+            }
+        }
+        System.out.println(name + ": " + "Your score is: " + score);
+        System.out.println("");
     }
 }
