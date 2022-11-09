fdd0d0004c4f9ff9a1405db69300ac939f5c0fd8
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-11-09 18:37:20 -0500

    U-TEST checkSeaBattle to check if player entered/won sea battle

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -265,4 +265,38 @@ public class PlayerTest {
         player.pickCard(Global.CardTypes.SOCERESS);
         assertEquals(player.getCard().getType(), Global.CardTypes.SOCERESS);
     }
+
+    @Test
+    public void TestCheckSeaBattle () {
+        Card c = new SeaBattle(Global.CardTypes.BATTLE);
+        c.setNumSkulls(3);
+
+        List<Dice> diceArray= new ArrayList<Dice>(8);
+        Dice d1 = new Dice();
+        d1.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d1);
+        Dice d2 = new Dice();
+        d2.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d2);
+        Dice d3 = new Dice();
+        d3.setDice(Global.DiceSide.SKULL);
+        diceArray.add(d3);
+        Dice d4 = new Dice();
+        d4.setDice(Global.DiceSide.PARROT);
+        diceArray.add(d4);
+        Dice d5 = new Dice();
+        d5.setDice(Global.DiceSide.GOLD);
+        diceArray.add(d5);
+        Dice d6 = new Dice();
+        d6.setDice(Global.DiceSide.DIAMOND);
+        diceArray.add(d6);
+        Dice d7 = new Dice();
+        d7.setDice(Global.DiceSide.SWORD);
+        diceArray.add(d7);
+        Dice d8 = new Dice();
+        d8.setDice(Global.DiceSide.MONKEY);
+        diceArray.add(d8);
+
+        assertTrue(player.checkSeaBattle(diceArray, c));
+    }
 }
