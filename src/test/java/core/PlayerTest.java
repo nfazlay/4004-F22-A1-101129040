a6efcfef15c4bb5512c67e87de538746faa8adfd
nfazlay <fazlayrabbi@cmail.carleton.ca>
2022-11-09 21:13:03 -0500

    U-TEST count score

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -4,6 +4,8 @@ import core.Player;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static core.Global.DiceSide.SKULL;
+import static core.Global.DiceSide.SWORD;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
@@ -179,40 +181,14 @@ public class PlayerTest {
 
     @Test
     public void testCountPoints () {
-        List<Dice> diceArray= new ArrayList<Dice>(8);
-        Dice d1 = new Dice();
-        d1.setDice(Global.DiceSide.SKULL);
-        diceArray.add(d1);
-        Dice d2 = new Dice();
-        d2.setDice(Global.DiceSide.SKULL);
-        diceArray.add(d2);
-        Dice d3 = new Dice();
-        d3.setDice(Global.DiceSide.SKULL);
-        diceArray.add(d3);
-        Dice d4 = new Dice();
-        d4.setDice(Global.DiceSide.PARROT);
-        diceArray.add(d4);
-        Dice d5 = new Dice();
-        d5.setDice(Global.DiceSide.GOLD);
-        diceArray.add(d5);
-        Dice d6 = new Dice();
-        d6.setDice(Global.DiceSide.DIAMOND);
-        diceArray.add(d6);
-        Dice d7 = new Dice();
-        d7.setDice(Global.DiceSide.SWORD);
-        diceArray.add(d7);
-        Dice d8 = new Dice();
-        d8.setDice(Global.DiceSide.MONKEY);
-        diceArray.add(d8);
+        int points;
+        player.pickCard(Global.CardTypes.GOLD_CARD);
 
-        Card c = new Card(Global.CardTypes.CHEST);
+        Global.DiceSide[] ds = new Global.DiceSide[]{SWORD,SWORD,SWORD,SWORD,SWORD,SKULL,SKULL,SKULL};
+        List<Dice> dices = Global.createDiceList(ds);
 
-        int points  = player.countPoints(diceArray, c);
-        assertEquals(300, points);
-
-        c = new Card(Global.CardTypes.CAPTAIN);
-        points  = player.countPoints(diceArray, c);
-        assertEquals(600, points);
+        points = player.countPoints(dices, player.getCard());
+        assertEquals(0, points);
     }
 
     @Test
